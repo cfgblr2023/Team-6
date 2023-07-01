d01d3c5436c2b43fcdc3d5701f7707bbf0ff11fe
raagzi <rakshakchinivar@gmail.com>
2023-07-01 22:52:04 +0530

    Templates added

--- lamplighting/testEnv/Scripts/activate.bat
+++ lamplighting/testEnv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\JPMC CFG2023\Team-6\lamplighting\testEnv
+set VIRTUAL_ENV=D:\JPMC\Team-6\lamplighting\testEnv
 
 if not defined PROMPT set PROMPT=$P$G
 
