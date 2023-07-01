4149271014fa4274d24e5eb8f4937bb40ffa680f
VarunBhattacharya <personnel.varunb@gmail.com>
2023-07-02 01:33:08 +0530

    added feedback

--- lamplighting/app_main/models.py
+++ lamplighting/app_main/models.py
@@ -14,6 +14,7 @@ class Course(models.Model):
     CourseName = models.CharField(max_length=25)
     CourseCategory = models.CharField(max_length=25)
     CourseDescription = models.CharField(max_length=100)
+    CourseFeedback = models.CharField(max_length=25)
     CourseMentor = models.CharField(max_length=25)
     CourseMentee = models.CharField(max_length=25)
 
