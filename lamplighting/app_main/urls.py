b2c1fd7f60e4f5ba18c68e3d65e9ea60bcab6185
Rakshith356 <rakshithchajjed@gmail.com>
2023-07-01 22:30:02 +0530

    Updated URLs

--- lamplighting/app_main/urls.py
+++ lamplighting/app_main/urls.py
@@ -4,4 +4,15 @@ from django.urls import path
 
 urlpatterns = [
     path('home/', view=home),
+    path('loginMentee/',view=loginMentee),
+    path('loginMentor/',view=loginMentor),
+    path('loginAdmin/',view=loginAdmin),
+    path('registerMentee/',view=registerMentee),
+    path('registerMentor/',view=registerMentor),
+    path('registerAdmin/',view=registerAdmin),
+    path('mentor/',view=mentor),
+    path('mentee/',view=mentee),
+    path('admin/',view=adminUser),
+    path('donate/',view=donate),
+    path('jobPosting/',view=jobPosting)
 ]
