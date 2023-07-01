4a02850299baaed717ed73881949e9f1ea7a925f
VarunBhattacharya <personnel.varunb@gmail.com>
2023-07-01 23:33:34 +0530

    db structure

--- lamplighting/app_main/admin.py
+++ lamplighting/app_main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Course, CourseRelation
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('Username', 'Name', 'Password', 'PhoneNumber', 'Permission', 'Email')
+
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('CourseID', 'CourseName', 'CourseCategory', 'CourseDescription', 'CourseMentor', 'CourseMentee')
+
+class CourseRelationAdmin(admin.ModelAdmin):
+    list_display = ('Mentor', 'Mentee', 'CourseStatus')
