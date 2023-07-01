4349245304d184a347ff4b621610e6580092dab5
VarunBhattacharya <personnel.varunb@gmail.com>
2023-07-01 20:58:58 +0530

    create users db

--- lamplighting/app_main/models.py
+++ lamplighting/app_main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    Username= models.CharField(max_length=25)
+    Name= models.CharField(max_length=25)
+    Password= models.CharField(max_length=100)
+    PhoneNumber= models.IntegerField(max_length=10)
+    Permission= models.CharField(max_length=25)
+    Email= models.CharField(max_length=25)
