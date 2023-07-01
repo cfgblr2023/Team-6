8681727b5513a86d3a601da19bcd31beb59dda43
Shubhabrata Ghosh <shubhabrata08@gmail.com>
2023-07-01 22:32:26 +0530

    Static files

--- lamplighting/app_main/urls.py
+++ lamplighting/app_main/urls.py
@@ -1,7 +1,11 @@
 from views import *
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('home/', view=home),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
