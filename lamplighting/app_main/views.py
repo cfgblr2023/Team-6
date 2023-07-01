89e44927ec579d03ae5f5c609e1a8c2c766c8f4a
Rakshith356 <rakshithchajjed@gmail.com>
2023-07-01 23:51:02 +0530

    Updated views and routes

--- lamplighting/app_main/views.py
+++ lamplighting/app_main/views.py
@@ -4,3 +4,39 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Home")
+
+def loginMentee(request):
+    return render(request,"mentee_login.html")
+
+def loginMentor(request):
+    return render(request,"mentor_login.html")
+
+def loginAdmin(request):
+    return render(request,"admin_login.html")
+
+def registerMentee(request):
+    return render(request,"mentee_register.html")
+
+def registerMentor(request):
+    return render(request,"mentor_register.html")
+
+def mentor(request):
+    return render(request,"mentor.html")
+
+def mentee(request):
+    return render(request,"mentee.html")
+
+def adminUser(request):
+    return render(request,"admin.html")
+
+def donate(request):
+    return render(request,"")
+
+def jobPosting(request):
+    return render(request,"")
+
+
+
+
+
+
