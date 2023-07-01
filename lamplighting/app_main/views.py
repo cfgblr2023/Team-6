2113dee4c5cdb152573cbcdcca845572a687cd29
Shubhabrata Ghosh <shubhabrata08@gmail.com>
2023-07-02 02:45:40 +0530

    Login fixed

--- lamplighting/app_main/views.py
+++ lamplighting/app_main/views.py
@@ -2,22 +2,25 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
-
+from .models import EndUser
 def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            # Redirect to a success page
+        try:
+            user = EndUser.objects.get(Username=username, Password=password)
+            # Authentication successful
+            request.session['user_id'] = user.id
+            # You can add any additional session data here if needed
             return HttpResponse("Success")
-        else:
-            # Show an error message
+        except EndUser.DoesNotExist:
+            # Authentication failed
             return HttpResponse("Failure")
 
-            # return render(request, 'accounts/login.html', {'error': 'Invalid username or password.'})
-    return render(request, 'accounts/login.html')
+            # messages.error(request, 'Invalid username or password.')
+
+    return render(request, 'home.html')
 
 # Create your views here.
 
