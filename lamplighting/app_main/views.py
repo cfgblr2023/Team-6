ae3a1c24198ea039ce7e1a1b3bdc52ba06e4fb04
Rakshith356 <rakshithchajjed@gmail.com>
2023-07-02 02:42:04 +0530

    updated views

--- lamplighting/app_main/views.py
+++ lamplighting/app_main/views.py
@@ -1,43 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-
-def login_view(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            # Redirect to a success page
-        else:
-            # Show an error message
-            return render(request, 'accounts/login.html', {'error': 'Invalid username or password.'})
-    return render(request, 'accounts/login.html')
-
 # Create your views here.
 
 def home(request):
     return render(request, "home.html")
 
-def loginUser(request):
-    return render(request,"loginUser.html")
-
 def loginMentee(request):
-    return render(request,"mentee/mentee_login.html")
+    return render(request,"mentee_login.html")
 
 def loginMentor(request):
-    return render(request,"Mentor/mentor_login.html")
+    return render(request,"mentor_login.html")
 
 def loginAdmin(request):
-    return render(request,"Admin/admin_login.html")
+    return render(request,"admin_login.html")
 
 def registerMentee(request):
-    return render(request,"mentee/mentee_register.html")
+    return render(request,"mentee_register.html")
 
 def registerMentor(request):
-    return render(request,"Mentor/mentor_register.html")
+    return render(request,"mentor_register.html")
 
 def mentor(request):
     return render(request,"mentor.html")
