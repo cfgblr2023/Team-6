b257a95f819d97a7fbc06f45913b86ebc88f0954
VarunBhattacharya <personnel.varunb@gmail.com>
2023-07-01 23:56:04 +0530

    normal admin changes

--- lamplighting/app_main/admin.py
+++ lamplighting/app_main/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import User, Course, CourseRelation
+from .models import EndUser, Course, CourseRelation
 
 # Register your models here.
-# admin.site.register(User)
+admin.site.register(EndUser)
 admin.site.register(Course)
 admin.site.register(CourseRelation)
